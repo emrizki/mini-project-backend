53002a98de1058240d9ae848d21a09a3df465867
M Rizki <emrizkidev@gmail.com>
2022-03-01 21:36:27 +0700

    add delete route and controller

--- controllers/assetController.js
+++ controllers/assetController.js
@@ -31,6 +31,21 @@ class assetController {
         res.status(500).json({ message: 'internal server error' });
       });
   }
+
+  static deleteAsset(req, res) {
+    Aset.destroy({ where: { id: req.params.id } })
+      .then((result) => {
+        console.log('result >>>>', result);
+        if (result === 1) {
+          res.status(200).json({ message: 'Aset has been deleted' });
+        } else {
+          res.status(404).json({ message: 'Aset not found' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: 'internal server error' });
+      });
+  }
 }
 
 module.exports = assetController;

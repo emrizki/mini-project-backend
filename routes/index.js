083580244e2d469bfed41e23f5b21db6f9aea984
M Rizki <emrizkidev@gmail.com>
2022-03-01 22:14:34 +0700

    update router and app.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { route } = require('express/lib/application');
 const assetController = require('../controllers/assetController');
 
 router.get('/welcome', (req, res) => {
